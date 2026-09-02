2c99843d846d2a877abb085f9eb8a8de3bbfcbf7
007shp18-dot <007shp18@gmail.com>
2026-09-02 16:16:39 +0900

    chore: add bounded HDB export bridge

---
+++ v2/apps/web/lib/singapore/hdb-download.server.ts
@@ -0,0 +1,1 @@
+fatal: path 'v2/apps/web/lib/singapore/hdb-download.server.ts' exists on disk, but not in 'HEAD'
---
+++ v2/apps/web/test/hdb-download.test.ts
@@ -0,0 +1,1 @@
+fatal: path 'v2/apps/web/test/hdb-download.test.ts' exists on disk, but not in 'HEAD'
